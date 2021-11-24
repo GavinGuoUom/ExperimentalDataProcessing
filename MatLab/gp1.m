711f276d4c021edf81f18532a49d49ba609c8cc7
Jianwei Guo <jianwei.guo@postgrad.manchester.ac.uk>
2021-11-24 12:46:24 +0000

    FPGA Added2

--- MatLab/gp1.m
+++ MatLab/gp1.m
@@ -1,6 +1,6 @@
-filename = 'RT-5kHz-04-uh.csv';% 5kHz
+% filename = 'RT-5kHz-04-uh.csv';% 5kHz
 % filename = 'RF-10.csv'; % Frequency test 1 @ 5kHz
-% filename = 'RF-11.csv'; % Frequency test 2 @ 5kHz
+filename = 'RF-11.csv'; % Frequency test 2 @ 5kHz
 % filename = 'RF-101.csv'; % Frequency test 3 @ 5kHz
 % filename = 'RF-500Hz-01.csv'; % Frequency test @ 500Hz
 % filename = 'RT-5kHz-01.csv'; % Healthy Dynamic test @ 5kHz
@@ -10,8 +10,8 @@ filename = 'RT-5kHz-04-uh.csv';% 5kHz
 metadata = readmatrix(filename);
 figure(3)
 plot(metadata(1, :), metadata(2, :))
-data1 = metadata;
-% data1 = metadata(:, metadata(1, :)<5&metadata(1, :)>0.5);
+% data1 = metadata;
+data1 = metadata(:, metadata(1, :)<1&metadata(1, :)>0.72);
 Fs = 5000;
 T = 1/Fs;
 L = size(data1, 2);
